73a3901756e93c78b4a5646e35e733912c4c8fde
Jean-David Pallares <jeandavid.pallares@gmail.com>
2026-02-20 03:27:47 -0500

    fix: proper distDir for Vercel monorepo deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,16 @@
-import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: path.resolve(__dirname, "dist"),
+    outDir: "dist",
+    emptyOutDir: true, 
   },
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8000",
+        target: "http://localhost:8000", 
         changeOrigin: true,
       },
     },
